1d841dfa2d2a962e4724c8fd813fa871155de312
jakegriffin9902 <jakegriffin9902@gmail.com>
2024-02-06 13:42:08 -0600

    1.0
    Sprint 1, featuring a demo map and a movable character

--- character1script.gd
+++ character1script.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var move_speed : float = 100
 
 func _physics_process(_delta):
+	
 	# Get input direction
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
