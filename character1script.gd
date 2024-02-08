efcad44b544f744b4385cee94b7ec590495b0e45
Nic <94198408+its-niceferrari@users.noreply.github.com>
2024-02-07 23:03:16 -0600

    1.1
    Sprint 1 Update 1
    -Added tracking camera for character
    -Added character animations

--- character1script.gd
+++ character1script.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0,1)
+
+@onready var animation_tree = $AnimationTree
+
+func _ready():
+	animation_tree.set("parameters/Move/blend_position", starting_direction)
 
 func _physics_process(_delta):
 	
@@ -10,9 +16,14 @@ func _physics_process(_delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
+	update_animation_parameters(input_direction)
+	
 	# Update velocity
 	velocity = input_direction * move_speed
-	print(velocity)
 	
 	# Move and Slide function uses velocity of character body to move character on map
 	move_and_slide()
+	
+func update_animation_parameters(move_input : Vector2):
+		if(move_input != Vector2.ZERO):
+			animation_tree.set("parameters/Move/blend_position", move_input)
