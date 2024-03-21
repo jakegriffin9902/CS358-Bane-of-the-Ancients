28b28e7409dba89c2862b642eca577fbcbb394ad
Nic <94198408+its-niceferrari@users.noreply.github.com>
2024-03-21 11:24:30 -0500

    3.1
    Fixed the logic for triggering Action 1 and added a text box for displaying game dialogue.

--- characters/player_script.gd
+++ characters/player_script.gd
@@ -1,29 +1,39 @@
 extends CharacterBody2D
 
+@export var can_move : bool = true
 @export var move_speed : float = 45
 @export var starting_direction : Vector2 = Vector2(0,1)
 
+@export var touch_action1 : bool = false
+@export var count_touch_action1 : float = 0
+
 @onready var animation_tree = $AnimationTree
+@onready var game_text = $CanvasLayer/GameText
+@onready var text_rect = $CanvasLayer/ColorRect
 
 var maxHealth=10
 var curHealth=10
+
 func _ready():
 	animation_tree.set("parameters/Move/blend_position", starting_direction)
+	game_text.visible = false
+	text_rect.visible = false
 
 func _physics_process(_delta):
-	# Get input direction
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+	if can_move:
+		# Get input direction
+		var input_direction = Vector2(
+			Input.get_action_strength("right") - Input.get_action_strength("left"),
+			Input.get_action_strength("down") - Input.get_action_strength("up")
+		)
 	
-	update_animation_parameters(input_direction)
+		update_animation_parameters(input_direction)
 	
-	# Update velocity
-	velocity = input_direction * move_speed
+		# Update velocity
+		velocity = input_direction * move_speed
 	
-	# Move and Slide function uses velocity of character body to move character on map
-	move_and_slide()
+		# Move and Slide function uses velocity of character body to move character on map
+		move_and_slide()
 	
 func update_animation_parameters(move_input : Vector2):
 		if(move_input != Vector2.ZERO):
@@ -31,4 +41,10 @@ func update_animation_parameters(move_input : Vector2):
 
 
 func _on_action_1_trigger_body_entered(body):
-	print("Character entered the trigger area!")
+	count_touch_action1 += 1
+	if count_touch_action1 != 1:
+		touch_action1 = true
+		can_move = false
+		game_text.visible = true
+		text_rect.visible = true
+		game_text.text = "Character entered the trigger area!"
