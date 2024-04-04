84d770d80d139b39311cbd49cb566093d5d12bc1
Nic <94198408+its-niceferrari@users.noreply.github.com>
2024-04-04 17:26:16 -0500

    4.2.2
    Adds animation between the dialogue interaction and the battle scene

--- Action1Trigger.gd
+++ Action1Trigger.gd
@@ -18,21 +18,56 @@ func Action1Trigger():
 		Do you wish to help?")
 		get_parent().get_node("Player").set("can_move", false)
 
-
 func _on_yes_pressed():
-	# We need to add more text to lead into combat
+	get_parent().get_node("Player").position = Vector2(180, 160)
+	
+	get_node("CanvasLayer/vbox/text").set_text("You approach the creatures.")
+	get_node("CanvasLayer/vbox/hbox/no").set_visible(false)
+	get_node("CanvasLayer/vbox/hbox/yes").set_visible(false)
+	get_node("CanvasLayer/vbox/hbox/ok").set_visible(false)	
+	
+	await get_tree().create_timer(3.0).timeout
+	
+	get_node("CanvasLayer/vbox/text").set_text(
+		"They suddenly become aggressive!")
+		
+	await get_tree().create_timer(3.0).timeout
+	
+	get_parent().get_node("Human carrier/Sprite2D").set_frame(1)
+	get_parent().get_node("Animal carrier/Sprite2D").set_frame(1)
+	await get_tree().create_timer(0.5).timeout
+	
+	get_parent().get_node("Human carrier/Sprite2D").set_frame(2)
+	get_parent().get_node("Animal carrier/Sprite2D").set_frame(2)
+	await get_tree().create_timer(3.0).timeout
+	
+	get_node("CanvasLayer").set_visible(false)
 	get_tree().change_scene_to_file("res://battletest.tscn")
-
 
 func _on_no_pressed():
 	get_node("CanvasLayer/vbox/text").set_text(
 		"As you pass the creatures by,
 		they succumb to their wounds.")
+		
+	get_node("CanvasLayer/vbox/hbox/ok").set_visible(false)
 	get_node("CanvasLayer/vbox/hbox/no").set_visible(false)
 	get_node("CanvasLayer/vbox/hbox/yes").set_visible(false)
-	get_node("CanvasLayer/vbox/hbox/ok").set_visible(true)
+		
+	await get_tree().create_timer(2.0).timeout
 	
+	get_parent().get_node("Human carrier/Sprite2D").set_frame(1)
+	get_parent().get_node("Animal carrier/Sprite2D").set_frame(1)
+	await get_tree().create_timer(0.5).timeout
+	
+	get_parent().get_node("Human carrier/Sprite2D").set_frame(2)
+	get_parent().get_node("Animal carrier/Sprite2D").set_frame(2)
+	await get_tree().create_timer(0.5).timeout
+	
+	get_parent().get_node("Human carrier/Sprite2D").set_frame(5)
+	get_parent().get_node("Animal carrier/Sprite2D").set_frame(5)
+	await get_tree().create_timer(2.0).timeout
 
+	get_node("CanvasLayer/vbox/hbox/ok").set_visible(true)
 
 func _on_ok_pressed():
 	get_parent().get_node("Player").set("can_move", true)
